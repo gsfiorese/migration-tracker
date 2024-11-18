120534e20fb87342e15a234ed6c8c99b5b4e6452
Gabriel Fiorese <gsfiorese@gmail.com>
2024-11-19 10:04:16 +1100

    Admin Welcome View up and running with filtering feature, button details not implemented

--- app/controllers/user_admin/welcome_controller.rb
+++ app/controllers/user_admin/welcome_controller.rb
@@ -1,4 +1,10 @@
-class UserAdmin::WelcomeController < ApplicationController
-  def index
+module UserAdmin
+  class WelcomeController < ApplicationController
+    def index
+      migration_controller = YearlyMigration::YearlyMigrationDataController.new
+      result = migration_controller.fetch_data(params)
+      @sheet_names = result[:sheet_names]
+      @data = result[:data]
+    end
   end
 end
