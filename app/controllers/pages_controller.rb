58b9ede10407fb8a2e13ee4d1df8afacd693aea9
Ashwin Almeida <almeidashwin@gmail.com>
2024-11-26 19:44:55 +1100

    footers at the bottom of the page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def employment
+  end
+
+  def contact
+  end
+
+  def employment
+  end
+
+  def resources
+  end
+
+  def privacy_policy
+  end
+
+  def terms_of_service
+  end
+end
