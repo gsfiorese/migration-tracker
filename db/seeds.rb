154dadfa0bde0708538462904e9ff8aae2a7d73d
Ashwin Almeida <almeidashwin@gmail.com>
2024-12-03 19:59:21 +1100

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(
+  first_name: "admin",
+  email: "admin@gmail.com",
+  password: "123456",
+  password_confirmation: "123456",
+  role: "admin" # Optional attribute, depending on your User model
+)
+
+User.create!(
+  first_name: "customer",
+  email: "customer@gmail.com",
+  password: "123456",
+  password_confirmation: "123456",
+  role: "member" # Optional attribute, depending on your User model
+)
